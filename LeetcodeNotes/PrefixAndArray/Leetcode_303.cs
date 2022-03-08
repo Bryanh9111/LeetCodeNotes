c4926b48dc463f9d011036ac9a7d6b92ba0ce000
zhhlbaw2011@gmail.com <zhhlbaw2011@gmail.com>
2022-03-07 23:28:23 -0500

    303 added

---
+++ LeetcodeNotes/PrefixAndArray/Leetcode_303.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetcodeNotes.PrefixAndArray
+{
+    /// <summary>
+    /// reduce the time complexity from O(N) to O(1): no for loop in SumRange
+    /// </summary>
+    class NumArray
+    {
+        private int[] preSum;
+
+        public NumArray(int[] nums)
+        {
+            preSum = new int[nums.Length + 1];
+            for (int i = 1; i < preSum.Length; i++)
+                preSum[i] = preSum[i - 1] + nums[i - 1];
+        }
+        /// <summary>
+        /// TC: O(1)
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns></returns>
+        public int SumRange(int left, int right)
+        {
+            return preSum[right + 1] - preSum[left];
+        }
+    }
+
+    class NumArray_SLOW
+    {
+        private int[] nums;
+
+        public NumArray_SLOW(int[] nums)
+        {
+            this.nums = nums;
+        }
+        /// <summary>
+        /// TC: O(N)
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns></returns>
+        public int SumRange(int left, int right)
+        {
+            int res = 0;
+            for (int i = left; i <= right; i++)
+                res += nums[i];
+
+            return res;
+        }
+    }
+}
--- LeetcodeNotes/Program.cs
+++ LeetcodeNotes/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using LeetcodeNotes.PrefixAndArray;
 
 namespace LeetcodeNotes
 {
@@ -6,7 +7,28 @@ namespace LeetcodeNotes
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Hello World!");
+            var watch1 = new System.Diagnostics.Stopwatch();
+            watch1.Start();
+            /////////////////
+            NumArray na = new NumArray(new int[] { -2, 0, 3, -5, 2, -1 });
+            na.SumRange(0, 2);
+            na.SumRange(2, 5);
+            na.SumRange(0, 5);
+            /////////////////
+            watch1.Stop();
+            Console.WriteLine($"Execution Time watch1: {watch1.ElapsedMilliseconds} ms");
+
+            var watch2 = new System.Diagnostics.Stopwatch();
+            watch2.Start();
+            /////////////////
+            NumArray_SLOW nas = new NumArray_SLOW(new int[] { -2, 0, 3, -5, 2, -1 });
+            nas.SumRange(0, 2);
+            nas.SumRange(2, 5);
+            nas.SumRange(0, 5);
+            /////////////////
+            watch2.Stop();
+            Console.WriteLine($"Execution Time watch2: {watch2.ElapsedMilliseconds} ms");
+            Console.ReadKey();
         }
     }
 }
