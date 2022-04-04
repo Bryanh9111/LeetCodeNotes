8f8eee27016944efc87361a2ef560661c2399d16
zhhlbaw2011@gmail.com <zhhlbaw2011@gmail.com>
2022-04-03 22:57:17 -0400

    O(1)删除数组末尾元素

---
+++ LeetcodeNotes/1-基础数据结构/1.4-数据结构设计/常数时间删除查找元素/Leetcode_380.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetcodeNotes.常数时间删除查找元素
+{
+    public class RandomizedSet
+    {
+        private List<int> nums;// 存储元素的值，取元素O(1)，删除最后一个元素O(1)
+        private Dictionary<int, int> valToIndex; // 记录每个元素对应在 nums 中的索引,用来交换元素到列表最后
+        public RandomizedSet()
+        {
+            nums = new List<int>();
+            valToIndex = new Dictionary<int, int>();
+        }
+        private void Swap(int index1, int index2)
+        {
+            int temp = nums[index1];
+            nums[index1] = nums[index2];
+            nums[index2] = temp;
+        }
+        public bool Insert(int val)
+        {
+            if (valToIndex.ContainsKey(val))
+                return false;// 若 val 已存在，不⽤再插⼊
+            valToIndex.Add(val, nums.Count);
+            nums.Add(val);
+            return true;
+        }
+
+        public bool Remove(int val)
+        {
+            if (!valToIndex.ContainsKey(val))
+                return false;
+
+            int index = valToIndex[val];// 先拿到 val 的索引
+            int last_index = nums.Count - 1;
+            valToIndex[nums[last_index]] = index; // 将最后⼀个元素对应的索引修改为 index
+            valToIndex.Remove(val);// 删除元素 val 对应的索引
+            Swap(index, last_index);// 交换 val 和最后⼀个元素
+            nums.RemoveAt(last_index);// 在数组中删除元素 val
+            return true;
+        }
+
+        public int GetRandom()
+        {
+            return nums[new Random().Next(0, int.MaxValue) % nums.Count];
+        }
+    }
+}
---
+++ LeetcodeNotes/1-基础数据结构/1.4-数据结构设计/常数时间删除查找元素/Leetcode_710.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetcodeNotes.常数时间删除查找元素
+{
+    public class Solution
+    {
+        private int sz;
+        private Dictionary<int, int> mapping;
+        private Random rand;
+        public Solution(int n, int[] blacklist)
+        {
+            rand = new Random();
+            mapping = new Dictionary<int, int>();
+            sz = n - blacklist.Length;// 最终数组中的元素个数
+
+            foreach (var b in blacklist)// 先将所有⿊名单数字加⼊ map
+                mapping.Add(b, 6666);  // 这⾥赋值多少都可以
+                                     // ⽬的仅仅是把键存进哈希表
+                                      // ⽅便快速判断数字是否在⿊名单内
+
+            int last = n - 1;// 最后⼀个元素的索引
+            foreach (var b in blacklist)
+            {
+                if (b >= sz)// 如果 b 已经在区间 [sz, N)
+                    continue;      // 可以直接忽略
+
+                while (mapping.ContainsKey(last))
+                    last--;// 跳过所有⿊名单中的数字
+
+                mapping[b] = last--;// 将⿊名单中的索引换到最后去
+            }
+                
+        }
+
+        public int Pick()
+        {
+            int index = rand.Next(0, sz) % sz;// 随机选取⼀个索引
+            if (mapping.ContainsKey(index))
+                return mapping[index];// 这个索引命中了⿊名单，
+                                      // 需要被映射到其他位置
+            return index;// 若没命中⿊名单，则直接返回
+        }
+    }
+}
