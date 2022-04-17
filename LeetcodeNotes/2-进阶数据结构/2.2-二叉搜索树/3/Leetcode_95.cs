1342bbefc5d8c83bb53b0390faa7bb5f607c7c43
zhhlbaw2011@gmail.com <zhhlbaw2011@gmail.com>
2022-04-17 19:04:52 -0400

    recall

---
+++ LeetcodeNotes/2-进阶数据结构/2.2-二叉搜索树/3/Leetcode_95.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetcodeNotes._2_进阶数据结构._2._2_二叉搜索树._3
+{
+    class Leetcode_95
+    {
+        public IList<TreeNode> GenerateTrees(int n)
+        {
+            if (n == 0)
+                return null;
+            // 构造闭区间 [1, n] 组成的 BST 
+            return Helper(1, n);
+        }
+        /* 构造闭区间 [lo, hi] 组成的 BST */
+        public IList<TreeNode> Helper(int low, int high)
+        {
+            IList<TreeNode> res = new List<TreeNode>();
+            if (low > high)
+            {
+                res.Add(null);
+                return res;
+            }
+            // 1、穷举 root 节点的所有可能。
+            for (int i = low; i <= high; i++)
+            {
+                // 2、递归构造出左右子树的所有合法 BST。
+                IList<TreeNode> leftTree = Helper(low, i - 1);
+                IList<TreeNode> rightTree = Helper(i + 1, high);
+                // 3、给 root 节点穷举所有左右子树的组合。
+                foreach (var left in leftTree)
+                {
+                    foreach(var right in rightTree)
+                    {
+                        // i 作为根节点 root 的值
+                        TreeNode root = new TreeNode(i);
+                        root.left = left;
+                        root.right = right;
+                        res.Add(root);
+                    }
+                }
+            }
+            return res;
+        }
+    }
+}
---
+++ LeetcodeNotes/2-进阶数据结构/2.2-二叉搜索树/3/Leetcode_96.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetcodeNotes._2_进阶数据结构._2._2_二叉搜索树._3
+{
+    /// <summary>
+    /// Time limit exceedeed
+    /// </summary>
+    class Leetcode_96
+    {
+        //左子树的组合数和右子树的组合数乘积就是n作为根节点时的 BST 个数
+        public int NumTrees(int n)
+        {
+            //计算闭区间 [1, n] 组成的 BST 个数
+            return Helper(1, n);
+        }
+        //计算闭区间 [lo, hi] 组成的 BST 个数
+        public int Helper(int low, int high)
+        {
+            //空区间，也就对应着空节点 null，虽然是空节点，但是也是一种情况，所以要返回 1 而不能返回 0
+            if (low > high)
+                return 1;
+
+            int res = 0;
+            for(int i = low; i <= high; i++)
+            {
+                // i 的值为根节点 root
+                int left = Helper(low, i - 1);
+                int right = Helper(i + 1, high);
+                // 左右子树的组合数乘积是 BST 的总数
+                res += left * right;
+            }
+            return res;
+        }
+    }
+
+    class Leetcode_96_fast
+    {
+        int[,] memo;
+        //左子树的组合数和右子树的组合数乘积就是n作为根节点时的 BST 个数
+        public int NumTrees(int n)
+        {
+            memo = new int[n + 1, n + 1];
+            //计算闭区间 [1, n] 组成的 BST 个数
+            return Helper(1, n);
+        }
+        //计算闭区间 [lo, hi] 组成的 BST 个数
+        public int Helper(int low, int high)
+        {
+            //空区间，也就对应着空节点 null，虽然是空节点，但是也是一种情况，所以要返回 1 而不能返回 0
+            if (low > high)
+                return 1;
+
+            // 查备忘录
+            if (memo[low, high] != 0)
+                return memo[low, high];
+
+            int res = 0;
+            for (int i = low; i <= high; i++)
+            {
+                // i 的值为根节点 root
+                int left = Helper(low, i - 1);
+                int right = Helper(i + 1, high);
+                // 左右子树的组合数乘积是 BST 的总数
+                res += left * right;
+            }
+            // 将结果存入备忘录
+            memo[low, high] = res;
+
+            return res;
+        }
+    }
+}
