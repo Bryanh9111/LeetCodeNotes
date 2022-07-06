0fc6d1ec39bc07640e42cce356ab1a87eb0e5a0f
zhhlbaw2011@gmail.com <zhhlbaw2011@gmail.com>
2022-07-06 17:51:06 -0400

    931 update

--- LeetcodeNotes/4-动态规划/4.1-动态规划核心原理/2- basecase和备忘录初始值怎么定/Leetcode_931.cs
+++ LeetcodeNotes/4-动态规划/4.1-动态规划核心原理/2- basecase和备忘录初始值怎么定/Leetcode_931.cs
@@ -88,4 +88,43 @@ namespace LeetcodeNotes._4_动态规划._4._1_动态规划核心原理._2__basec
             return Math.Min(a, Math.Min(b, c));
         }
     }
+    internal class Leetcode_931_dptable
+    {
+        public int MinFallingPathSum(int[][] matrix)
+        {
+            int n = matrix.Length;
+
+            if (n == 0) return 0;
+
+            int[,] dp = new int[n, n];
+            for (int j = 0; j < n; j++)
+                dp[0, j] = matrix[0][j];
+            for (int i = 1; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    dp[i, j] = 10001;
+
+            for (int i = 1; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (j == 0)
+                        dp[i, j] = Math.Min(dp[i, j], matrix[i][j] + Math.Min(dp[i - 1, j], dp[i - 1, j + 1]));
+                    else if (j == n - 1)
+                        dp[i, j] = Math.Min(dp[i, j], matrix[i][j] + Math.Min(dp[i - 1, j], dp[i - 1, j - 1]));
+                    else
+                        dp[i, j] = Math.Min(dp[i, j], matrix[i][j] + MinHelper(dp[i - 1, j], dp[i - 1, j - 1], dp[i - 1, j + 1]));
+                }
+            }
+            int res = 10001;
+            for (int j = 0; j < n; j++)
+                res = Math.Min(res, dp[n - 1, j]);
+
+            return res;
+        }
+
+        public int MinHelper(int a, int b, int c)
+        {
+            return Math.Min(a, Math.Min(b, c));
+        }
+    }
 }
